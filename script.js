00c23b320c4d780fb6a776b5d2fcea8ba7ea09db
Caswell Orr <caswell.orr@gmail.com>
2021-11-21 15:50:38 -0600

    JS pseudo-code

---
+++ script.js
@@ -0,0 +1,7 @@
+// When i search a city in the search bar, the current weather should appear in the JUMBOTRON and the 5 day forecast to appear in the cards - one day per card
+
+// The information in the cards should include: the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// the jumbotron should include: the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+// the UV index should present a color that indicates whether the conditions are favorable, moderate, or severe
